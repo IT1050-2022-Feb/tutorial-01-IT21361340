5e1be8f401c7741be3df976d71c609695e21c3b7
IT21361340 <101385520+IT21361340@users.noreply.github.com>
2022-03-11 13:07:51 +0530

    Tutorial 1 (Exercise 1)

--- Tute01.c
+++ Tute01.c
@@ -4,7 +4,20 @@
 
 #include <stdio.h>
 
-int main() {
+int main(void) {
+   int mark1, mark2;
+   int total;
+   float avg;
+  
+   printf("Enter mark 1: ");
+   scanf("%d",&mark1);
+   printf("Enter mark 2: ");
+   scanf("%d",&mark2);
+   
+   total = mark+mark2;
+   avg= (mark1+mark2)/2.0;
+   printf("Total:%d",total);
+   printf("Average:%.2f\n",avg);
   
   return 0;
 }
